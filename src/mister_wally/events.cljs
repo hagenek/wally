193b74eae95ecf29066d697b8b50bd9bafdba71d
Georg Ekeberg <georghagen@gmail.com>
2021-12-07 12:01:17 +0100

    added logo and some buttons

--- src/mister_wally/events.cljs
+++ src/mister_wally/events.cljs
@@ -2,20 +2,24 @@
   (:require
    [re-frame.core :as re-frame]
    [mister-wally.db :as db]
-   [day8.re-frame.tracing :refer-macros [fn-traced]]
-   ))
+   [day8.re-frame.tracing :refer-macros [fn-traced]]))
 
 (re-frame/reg-event-db
  ::initialize-db
  (fn-traced [_ _]
-   db/default-db))
+            db/default-db))
 
 (re-frame/reg-event-fx
-  ::navigate
-  (fn-traced [_ [_ handler]]
-   {:navigate handler}))
+ ::navigate
+ (fn-traced [_ [_ handler]]
+            {:navigate handler}))
 
 (re-frame/reg-event-fx
  ::set-active-panel
  (fn-traced [{:keys [db]} [_ active-panel]]
-   {:db (assoc db :active-panel active-panel)}))
+            {:db (assoc db :active-panel active-panel)}))
+
+(re-frame/reg-event-fx
+ ::set-active-user
+ (fn-traced [{:keys [db]} [_ username]]
+            {:db (assoc db :name username)}))
--- src/mister_wally/views.cljs
+++ src/mister_wally/views.cljs
@@ -1,28 +1,50 @@
 (ns mister-wally.views
   (:require
    [re-frame.core :as re-frame]
+   [reagent.core :as r]
    [mister-wally.events :as events]
    [mister-wally.routes :as routes]
    [mister-wally.subs :as subs]
-   ["@smooth-ui/core-sc" :refer [Button]]))
+   ["@smooth-ui/core-sc" :refer [Input
+                                 Button
+                                 Box]]))
 
 
 ;; home
 
+
+(defn send-receive []
+  [:> Box {:display "flex"
+           :flex-direction "column"}
+   [:img {:alt "Nanocoin logo"
+          :className "nano-logo"
+          :src "img/nano/symbol-blue.svg"}]
+   [:> Box {:flex-direction "row" :justify-content "space-between"}
+    [:> Button "Send"]
+    [:> Button "Receive"]]])
+
+
+(defn set-username []
+  (let [username (r/atom "")]
+    [:div
+     [:> Input {:type "text" :label "Username" :value @username}]
+     [:> Button {:on-click #(re-frame/dispatch [::events/set-active-user @username])} "Set username"]]))
+
 (defn home-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:> Button {:variant "primary"} "Click me!"]
+     [send-receive]
+     [set-username]
      [:h1
       (str "Hello from " @name ". This is the Home Page.")]
      [:div
       [:a {:on-click #(re-frame/dispatch [::events/navigate :about])}
        "go to About Page"]]]))
 
+
 (defmethod routes/panels :home-panel [] [home-panel])
 
 ;; about
-
 (defn about-panel []
   [:div
    [:h1 "This is the About Page."]
